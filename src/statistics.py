6c272efa711a1fbf89c8c5aa118ccbc2cad7838c
Joel Gaspar <jgaspar@insulet.com>
2022-02-09 22:52:34 -0700

    se agega el indicador de porcentaje de inmuebles con estacionmiento por vecindario

--- src/statistics.py
+++ src/statistics.py
@@ -5,12 +5,16 @@ def obtener_estadistica():
     rentas_mas_altas = obtener_colonia_renta_mas_alta()
     mas_casas_renta = obtener_colonias_mas_rentas()
     habitaciones_promedio_vecindario, vecindario = obtener_habitaciones_promedio_vecindario()
+    porcentaje_estacionamiento = obtener_porcentaje_estacionamiento_vecindario()
     print(f"La colonia con la renta promedio mas elevada es: { rentas_mas_altas[0][1] } , con una renta promedio de: ${ rentas_mas_altas[0][0] } \n\n\n")
     print("Las 10 colonias con mas inmuebles en renta son: \n")
     for element in mas_casas_renta:
         print(f"- {element[1]}: {element[0]} en renta")
     print('')
-    print(f"El promedio de habitaciones en {vecindario} es de {habitaciones_promedio_vecindario[0][0]} ")
+    print(f"El promedio de habitaciones en {vecindario} es de {habitaciones_promedio_vecindario[0][0]: .2f} \n\n")
+    print("El promedio de inmuebles que cuentan con estacionamiento por cada vecindacio es de: ")
+    for element in porcentaje_estacionamiento:
+        print(f"- {element[1]}: {element[0]:.2f}% de inmuebles que cuentan con estacionamiento ")
 
 def obtener_colonia_renta_mas_alta():
     query = "SELECT AVG(PRECIO) as promedio, nombre FROM 'RentasVecindarios' group by nombre order by promedio desc limit 1"
@@ -29,3 +33,9 @@ def obtener_habitaciones_promedio_vecindario(vecindario='JACARANDAS'):
     conn = engine.connect()
     resultado = conn.execute(query).fetchall()
     return resultado, vecindario
+
+def obtener_porcentaje_estacionamiento_vecindario():
+    query = "SELECT  CAST(COUNT(IIF(estacionamiento=1, 1, null)) AS FLOAT) / COUNT(estacionamiento)*100 as porcentaje_estacionamiento, nombre FROM 'RentasVecindarios' group by nombre limit 20"
+    conn = engine.connect()
+    resultado = conn.execute(query).fetchall()
+    return resultado
